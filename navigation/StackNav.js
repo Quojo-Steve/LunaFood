8b5830994a1b4e9031f69632fae034ecfcd365da
Quojo-Steve <quojosteve@gmail.com>
2024-10-01 12:14:42 +0000

    Added onboarding page

--- navigation/StackNav.js
+++ navigation/StackNav.js
@@ -1,21 +1,56 @@
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from "../screens/OnboardingScreen";
 import Carousel from "../screens/Carousel";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Ensure AsyncStorage is imported
 
-export default StackNav = () => {
-  const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+
+const StackNav = () => {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   // Define an async function inside the effect
+  //   const checkFirstLaunch = async () => {
+  //     try {
+  //       const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+  //       if (appData == null) {
+  //         setIsAppFirstLaunched(true);
+  //         await AsyncStorage.setItem("isAppFirstLaunched", "false");
+  //       } else {
+  //         setIsAppFirstLaunched(false);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error checking first launch status", error);
+  //     }
+  //   };
+
+  //   // Call the async function
+  //   checkFirstLaunch();
+  // }, []); // The empty array means this effect runs only once on component mount
+
   return (
-    <Stack.Navigator
-      initialRouteName="carousel"
-      screenOptions={{ headerShown: false }}
-    >
+    // isAppFirstLaunched !== null && (
+    //   <Stack.Navigator screenOptions={{ headerShown: false }}>
+    //     {isAppFirstLaunched && (
+    //       <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+    //     )}
+    //     <Stack.Screen name="caro" component={Carousel} />
+    //   </Stack.Navigator>
+    // )
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
-        name="carousel"
+        name="OnboardingScreen"
+        component={OnboardingScreen}
+        options={{ gestureEnabled: false }}
+      />
+      <Stack.Screen
+        name="caro"
         component={Carousel}
-        options={{
-          gestureEnabled: false,
-          // headerLeft: () => null,
-        }}
+        options={{ gestureEnabled: false }}
       />
     </Stack.Navigator>
   );
 };
+
+export default StackNav;
